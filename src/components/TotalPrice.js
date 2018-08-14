7ecd19ab33654da491e7111cc00d3e6613342cc7
Jelename <yelena.medenica@gmail.com>
2018-08-14 15:19:55 +0200

    total price changes

--- src/components/TotalPrice.js
+++ src/components/TotalPrice.js
@@ -14,14 +14,14 @@ class TotalPrice extends React.PureComponent {
             <div>
                 <h1>Total Price is:</h1>
                 <h2>{this.getTotalPrice()}</h2>
-                <h4>Would you like to have a turbo-drone delivery?</h4>
+                {/* <h4>Would you like to have a turbo-drone delivery?</h4>
                 <p>That is additional 10% on top of your pizza price.</p>
                 <input
                     name='turbo-drone'
                     type='checkbox'
                     //checked={}
                     onChange={this.handleChange}
-                /> Add turbo-drone delivery
+                /> Add turbo-drone delivery */}
             </div>
         )
     }
